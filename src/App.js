8c809e7b362abe6771c18ce9b5a6fc0e74e3dc93
JohnMutemi <jmtutorsalp@gmail.com>
2024-07-17 03:03:03 +0300

    commit all updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Register from './components/Register';
+import AdminDashboard from './components/AdminDashboard';
+import DoctorDashboard from './components/DoctorDashboard';
+import PatientDashboard from './components/PatientDashboard';
+import Profile from './components/Profile';
+import ProfileLayout from './components/ProfileLayout'; // Import ProfileLayout
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route path="/profile" element={<ProfileLayout />}>
+          <Route path=":user_id" element={<Profile />} />
+          <Route path=":user_id/update" element={<Profile />} />
+        </Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
